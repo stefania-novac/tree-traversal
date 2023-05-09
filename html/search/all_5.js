5014c9cbae05bc1420e50cfd1b0f760bfaceae0d
ste3h14 <ste3h14@kali>
2023-05-09 20:04:07 +0300

    Generated DoxyGen documentation

---
+++ html/search/all_5.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['tree_0',['Tree',['../classtree_1_1Tree.html',1,'tree']]],
+  ['tree_20traversal_1',['Tree traversal',['../md_README.html',1,'']]]
+];
--- tree.py
+++ tree.py
@@ -62,27 +62,46 @@ class Tree:
             return self._find(data, node.right)
 
     def deleteTree(self):
-        # TODO 1
+        """ Method for delete tree """
         self.root = None
 
     def printTree(self):
-        # TODO 1
+        """ Method for print tree """
         if self.root is not None:
             self._printInorderTree(self.root)
 
     def _printInorderTree(self, node):
-        # TODO 1
+        """ Method for print inorder tree
+
+        Args:
+            Node: node to start with
+        """
         if node is not None:
             self._printInorderTree(node.left)
             print(str(node.data) + ' ')
             self._printInorderTree(node.right)
 
     def _printPreorderTree(self, node):
-        # TODO 2
-        pass
+        """ Method for print preorder tree
+
+        Args:
+            Node: node to start with
+        """
+        if node is not None:
+            print(str(node.data) + ' ')
+            self._printPreorderTree(node.left)
+            self._printPreorderTree(node.right)
+
 
     def _printPostorderTree(self, node):
-        # TODO 2
-        pass
+        """ Method for print postorder tree
+
+        Args:
+            Node: node to start with
+        """
+        if node is not None:
+            self._printPostorderTree(node.left)
+            self._printPostorderTree(node.right)
+            print(str(node.data) + ' ')
 
 
